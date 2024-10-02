9c416c37dc4576357a0d093ca0f2cef9abe945ae
aurora0x27 <qingfengzl0831@163.com>
2024-10-02 19:13:21 +0800

    add headers for header files

---
+++ include/qqmusic/album.h
@@ -0,0 +1,10 @@
+//-------------qqmusic/album.h---------------
+//            api about alnums
+#ifndef ALBUM_H
+#define ALBUM_H
+
+namespace qqmusic {
+
+} // namespace qqmusic
+
+#endif
---
+++ include/qqmusic/data.h
@@ -0,0 +1,10 @@
+//-------------qqmusic/data.h---------------
+//           api data and cache
+#ifndef DATA_H
+#define DATA_H
+
+namespace qqmusic {
+
+} // namespace qqmusic
+
+#endif
