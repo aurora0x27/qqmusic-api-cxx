c1e8bc08b78fa9cfea2f5f571859a3d655be6282
aurora0x27 <gsw2121497019@163.com>
2024-10-08 15:41:35 +0800

    add cpp files and file headers

---
+++ utils/include/utils/qrcdecoder.h
@@ -0,0 +1,12 @@
+//-------------utils/qrcdecoder.h---------------
+//       decode qrc lyric files
+#ifndef QRC_DECODER_H
+#define QRC_DECODER_H
+
+namespace qqmusic::utils
+{
+
+}
+// namespace qqmusic::utils
+
+#endif
