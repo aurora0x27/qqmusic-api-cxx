4888274bc3b59cb51ad9481e751b622d9ae1e1d1
aurora0x27 <gsw2121497019@163.com>
2024-10-09 21:21:06 +0800

    feat/api/error: initialize enum class result

--- include/qqmusic/error.h
+++ include/qqmusic/error.h
@@ -3,6 +3,12 @@
 //            global error info
 namespace qqmusic
 {
+    enum class result
+    {
+        network_connect_ok,
+        network_connect_timeout,
 
+        mem_alloc_failure
+    };
 }
 // namespace qqmusic
