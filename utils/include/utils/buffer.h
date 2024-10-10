4d901c5fe5938cb0de73988f43d7b0f0fc0e0de9
aurora0x27 <gsw2121497019@163.com>
2024-10-10 10:32:19 +0800

    feat/utils/buffer: change char* to uint8_t* and add clean function

--- utils/include/utils/buffer.h
+++ utils/include/utils/buffer.h
@@ -4,25 +4,27 @@
 #define BUFFER_H
 
 #include <stdio.h>
+#include <stdint.h>
 
 namespace qqmusic::utils
 {
     class buffer
     {
     public:
-                buffer       ();
-                ~buffer      ();
-        size_t  get_size     ();
-        char*   get_head     ();
-        size_t  buf_append   (void*  src_data_buf, 
-                              size_t data_block_size, 
-                              size_t data_block_num, 
-                              void*  dest_data_buf);
+                 buffer       ();
+                 ~buffer      ();
+        size_t   get_size     ();
+        uint8_t* get_head     ();
+        size_t   buf_append   (void*  src_data_buf, 
+                               size_t data_block_size, 
+                               size_t data_block_num, 
+                               void*  dest_data_buf);
+        void     buf_clean    ();
     private:
         struct buf_struct
         {
-            char* head;
-            int   size;
+            uint8_t* head;
+            int      size;
         }* data;
         void builtin_memcpy(void* dest, void* src, size_t size);
     };
