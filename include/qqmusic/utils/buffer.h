0cbb8514f68b2cc576d912f14e88ef1cff107b8d
aurora0x27 <gsw2121497019@163.com>
2024-10-27 14:46:33 +0800

    change qrc_decoder file name and export utils

---
+++ include/qqmusic/utils/buffer.h
@@ -0,0 +1,6 @@
+//-----------qqmusic/utils/buffer.h------------
+//-----------export utils/buffer.h-------------
+#ifndef QQMUSIC_UTILS_BUFFER_H
+#define QQMUSIC_UTILS_BUFFER_H
+#include <utils/buffer.h>
+#endif // !QQMUSIC_UTILS_BUFFER_H
---
+++ include/qqmusic/utils/credential.h
@@ -0,0 +1,6 @@
+//-----------qqmusic/utils/credential.h------------
+//-----------export utils/credential.h-------------
+#ifndef QQMUSIC_UTILS_CREDENTIAL_H
+#define QQMUSIC_UTILS_CREDENTIAL_H
+#include <utils/credential.h>
+#endif // !QQMUSIC_UTILS_CREDENTIAL_H
